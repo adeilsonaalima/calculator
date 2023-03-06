6fe8f7ff22660b994a9fbf5a3a0a1870f8d1de92
Adeilson Á. A. Lima <adeilson.afix@gmail.com>
2023-03-06 09:41:12 -0300

    feat: calculator

---
+++ script.js
@@ -0,0 +1,50 @@
+const buttons = document.querySelectorAll(".buttons__btn");
+const visor_maxlength = document.getElementById("visor");
+
+buttons.forEach((button) => {
+    button.addEventListener("click", (e) => {
+        handler(button);
+    });
+});
+
+function handler(button) {
+    switch (button.value) {
+        case "C":
+            clear();
+            break;
+        case "CE":
+            backspace();
+            break;
+        case "=":
+            equal();
+            break;
+        default:
+            num(button);
+    }
+}
+
+function clear() {
+    const visor = document.getElementById("visor");
+    visor.value = "";
+}
+
+function backspace() {
+    const visor = document.getElementById("visor");
+    visor.value = visor.value.slice(0, -1);
+}
+
+function equal() {
+    try {
+        const visor = document.getElementById("visor");
+        visor.value = eval(visor.value);
+    } catch (error) {
+        alert(error);
+    }
+}
+
+function num(button) {
+    const visor = document.getElementById("visor");
+    visor.value = visor.value + button.value;
+}
+
+uma simples calculadora
